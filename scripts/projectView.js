706d397cabcf20a302680864c18633f396194f9a
Duane Rush <desertduane@yahoo.com>
2017-08-23 21:10:32 -0700

    constructor and handelbars added and working projectData undefined for some reason

---
+++ scripts/projectView.js
@@ -0,0 +1,23 @@
+'use strict';
+var projects = [];
+
+function Project(projectDataObj) {
+  this.projectUrl = projectDataObj.projectUrl;
+  this.title = projectDataObj.title;
+  this.imgUrl = projectDataObj.imgUrl;
+}
+Project.prototype.toHtml = function() {
+  var template = $('#projects-template').html();
+  var templateRender = Handlebars.compile(template);
+
+  return templateRender(this);
+
+};
+
+projectData.forEach(function(projectObject) {
+  projects.push(new Project(projectObject))
+});
+
+projects.forEach(function(project) {
+  $('#projects').append(project.toHtml());
+})
