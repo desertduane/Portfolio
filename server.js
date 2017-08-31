e8297e4153f7cdb1f09cc06f1fdf68abf49bd0f5
Duane Rush <desertduane@yahoo.com>
2017-08-30 20:35:39 -0700

    working on deploying

--- server.js
+++ server.js
@@ -1,22 +1,30 @@
 'use strict';
+
 const pg = require('pg');
 const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
+const requestProxy = require('express-request-proxy');
 const PORT = process.env.PORT || 3000;
 const app = express();
-const conString = 'postgres://localhost:5432';
-const client = new pg.Client('conString');
-client.connect();
 
-//middleware stuff
+const conString = process.env.DATABASE_URL || 'postgres://localhost:5432';
+const client = new pg.Client(conString);
+client.connect();
+client.on('error', err => console.error(err));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('./public'));
 
-app.get('/home', function(request, response){
-  response.sendFile('index.html', {root: './public'});
-});
-app.get('/projects', function(request, response){
-  
-})
+
+function proxyGitHub(request, response) {
+  (requestProxy({
+    url: `https://api.github.com/${request.params[0]}`,
+    headers: {Authorization: `token ${process.env.GITHUB_TOKEN}`}
+  }))(request, response);
+}
+
+app.get('/github/*', proxyGitHub);
+
+app.get('/', (request, response) => response.sendFile('index.html', {root: './public'}));
